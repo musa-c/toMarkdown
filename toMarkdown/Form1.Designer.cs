9702f84bbafa48f955a6aa5cde3a431faa91fb5f
Musa Civelek <musacivelekx@gmail.com>
2023-04-21 04:16:01 +0300

    headline confirigues

---
+++ toMarkdown/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace toMarkdown
+{
+    partial class toMarkdown
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 450);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // toMarkdown
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "toMarkdown";
+            this.Text = "toMarkdown";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ toMarkdown/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace toMarkdown
+{
+    public partial class toMarkdown : Form
+    {
+
+        public toMarkdown()
+        {
+            InitializeComponent();
+        }
+
+        int lastChangedLine = -1; // son değişen satırın indeksi, -1 ile başlatılır
+        //bool isTextChanged = false; // metinde değişiklik olup olmadığını kontrol eder
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            lastChangedLine = richTextBox1.GetLineFromCharIndex(richTextBox1.GetFirstCharIndexOfCurrentLine());
+
+            if (lastChangedLine != -1 && richTextBox1.Lines.Length > lastChangedLine)
+            {
+                string text = richTextBox1.Lines[lastChangedLine];
+                Debug.WriteLine(lastChangedLine);
+                Debug.WriteLine(lastChangedLine + ": " + text.StartsWith("###"));
+                //Debug.WriteLine(text);
+                // Kontrolde # karakteri var mı diye kontrol et
+
+                if (text.StartsWith("#"))
+                {
+                    if (text.StartsWith("#"))
+                    {
+                        Heading(text, 32);
+                    }
+                    if (text.StartsWith("##"))
+                    {
+                        Heading(text, 24);
+                    }
+                    if (text.StartsWith("###"))
+                    {
+                        Heading(text, 18);
+                    }
+                }
+                else DefaultText(text);
+
+            }
+
+        }
+
+        private void Heading(string text, float fontSize)
+        {
+            // Tüm metni büyük harfe dönüştür
+            richTextBox1.SelectionLength = text.Length;
+            richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont.FontFamily, fontSize, richTextBox1.SelectionFont.Style);
+            //richTextBox1.Select(richTextBox1.Text.Length, 0); // metin sonuna atar kullanıcıyı
+        }
+
+        private void DefaultText(string text)
+        {
+            //richTextBox1.SelectionStart = richTextBox1.GetFirstCharIndexFromLine(lastChangedLine);
+            richTextBox1.SelectionLength = text.Length;
+            richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont.FontFamily, 12, richTextBox1.SelectionFont.Style);
+            //richTextBox1.SelectionStart = richTextBox1.Text.Length;
+        }
+
+    }
+}
